98b49a98c362727ffe7563d616e6a6b5e592c398
David Bronfen <davidbronfen@gmail.com>
2021-09-14 19:32:08 +0300

    Implement angular material (#8)
    * Implement angular material
    
    * Remove unused material module
    
    Co-authored-by: David Bronfen <david.bronfen@beaconcure.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
