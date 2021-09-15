2a32168a584412fc73680c44b094ce9b39867ebe
David Bronfen <david.bronfen@beaconcure.com>
2021-09-15 14:07:51 +0300

    Initialize google api request to search books

--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
+const https = require('https');
 require('dotenv').config();
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+app.get('/search-books', (req, res) => {
+    const data = [];
+    const options = {
+        hostname: 'www.googleapis.com',
+        path: `/books/v1/volumes?q=${req.query.searchTerm}&key=${process.env.GOOGLE_API_KEY}`,
+        method: 'GET',
+    }
+
+    const googleRequest = https.request(options, searchResponse => {
+        console.log(`statusCode: ${searchResponse.statusCode}`)
+
+        searchResponse.on('data', chunk => {
+            data.push(chunk);
+        }).on('end', () => {
+            const buffer = Buffer.concat(data);
+            res.json(JSON.parse(buffer.toString()));
+        });
+    })
+
+    googleRequest.on('error', error => {
+        console.error(error)
+    });
+
+    googleRequest.end();
 })
 
 app.listen(process.env.PORT, () => {
