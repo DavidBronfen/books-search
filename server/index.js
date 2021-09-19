9f846fda80ee8d5d80555956143e941bedc4aa7c
David Bronfen <davidbronfen@gmail.com>
2021-09-19 17:46:27 +0300

    set application to deploy (#19)
    * Updated .gitignore
    
    * Setup server port and remove log to console
    
    * Setup server and client for project build and deploy
    
    Co-authored-by: David Bronfen <david.bronfen@beaconcure.com>

--- server/index.js
+++ server/index.js
@@ -3,8 +3,15 @@ const app = express();
 const https = require('https');
 const cors = require("cors");
 require('dotenv').config();
+const port = process.env.PORT || 8080;
 
 app.use(cors());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 app.get('/search-books', (req, res) => {
     const data = [];
@@ -37,8 +44,8 @@ app.get('/search-books', (req, res) => {
     googleRequest.end();
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 })
 
 const getBooksList = (responseObject) => {
