cae07f2dfc45ff8d9e8022a3c8b7d4c4fb491a7a
David Bronfen <david.bronfen@beaconcure.com>
2021-11-11 14:27:11 +0200

    Added book-details component and connect it to the dialog

---
+++ client/src/app/feed/components/book-details/book-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BookDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/feed/feed.module.ts
+++ client/src/app/feed/feed.module.ts
@@ -6,6 +6,7 @@ import { StoreModule } from '@ngrx/store';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { FeedRoutingModule } from './feed-routing.module';
 import { SearchModule } from '../search/search.module';
@@ -15,6 +16,7 @@ import { ImageDirective } from './directives/image.directive';
 import { FeedComponent } from './components/feed/feed.component';
 import { BooksListComponent } from './components/books-list/books-list.component';
 import { BookCardComponent } from './components/book-card/book-card.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
 
 import { BooksEffect } from './store/books.effect';
 import { reducer } from './store/books.reducer';
@@ -26,15 +28,17 @@ import { reducer } from './store/books.reducer';
     BooksListComponent,
     BookCardComponent,
     ImageDirective,
+    BookDetailsComponent,
   ],
   imports: [
     CommonModule,
     FeedRoutingModule,
     MatCardModule,
     MatButtonModule,
+    MatDialogModule,
     StoreModule.forFeature('books', reducer),
     EffectsModule.forFeature([ BooksEffect ]),
     SearchModule,
-  ]
+  ],
 })
 export class FeedModule { }
