871d35a5faccbb7e585f65cfd5094a3e68ea277c
David Bronfen <david.bronfen@beaconcure.com>
2021-11-12 09:19:21 +0200

    Added wishlist layout and style

--- client/src/app/wishlist/components/wishlist/wishlist.component.ts
+++ client/src/app/wishlist/components/wishlist/wishlist.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { IWishListBookItemModel } from '../../models/wishlist.model';
+import { getWishlist } from '../../store/wishlist.reducer';
+import { RemoveFromWishlist } from '../../store/wishlist.actions';
+import { getName } from '../../../auth/store/auth.reducer';
 
 @Component({
   selector: 'app-wishlist',
@@ -7,10 +14,18 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WishlistComponent implements OnInit {
+  placeholderImage = 'assets/no_image.jpg';
+  wishlist$: Observable<IWishListBookItemModel[]>
+  name$: Observable<string>;
 
-  constructor() { }
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
+    this.wishlist$ = this.store.select(getWishlist);
+    this.name$ = this.store.select(getName);
   }
 
+  removeToWishList(id: string) {
+    this.store.dispatch(RemoveFromWishlist({id}));
+  }
 }
--- client/src/app/wishlist/wishlist.module.ts
+++ client/src/app/wishlist/wishlist.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+
 import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { WishlistRoutingModule } from './wishlist-routing.module';
 import { reducer } from './store/wishlist.reducer';
@@ -16,6 +20,9 @@ import { reducer } from './store/wishlist.reducer';
     WishlistRoutingModule,
     StoreModule.forFeature('wishlist', reducer),
     EffectsModule.forFeature([]),
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
   ]
 })
 export class WishlistModule {}
